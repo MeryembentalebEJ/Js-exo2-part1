5b29749215d3d2dbf15e6dd85fd7815e92573226
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-04-07 09:19:38 +0200

    js-exo2-part1

---
+++ main.js
@@ -0,0 +1,6 @@
+var nom =  'Meryem';
+var prénom = 'Bentaleb El Jadil';
+var ville = 'Paris';
+var identité = 'Nom : ' + nom + '\n' + 'prénom : ' + prénom + '\n' + 'ville : ' + ville ;
+alert(identité);
+console.log(identité)
